f71de5ebdc935739d9c83410788989e388978a3f
Jhonatan Silva <jhonatansilva@id.uff.br>
2023-08-08 02:29:23 -0300

    Created pipes and styles's table

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,10 @@
+import { Observable, catchError, of } from 'rxjs';
 // courses.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
 import { CoursesService } from '../services/courses.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -9,19 +12,26 @@ import { CoursesService } from '../services/courses.service';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {}
-
-  ngOnInit(): void {
-    this.coursesService.getCourses().subscribe(
-      courses => {
-        this.courses = courses;
-      },
-      error => {
-        console.error('Error loading courses:', error);
-      }
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.getCourses().pipe(
+      catchError((error) => {
+       this.onError('Erro ao carregar cursos.')
+        return of([]);
+      })
     );
   }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  ngOnInit(): void {}
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Course } from '../courses/course';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
-  apiUrl = 'api/courses'; // Caminho relativo ao arquivo JSON
+  private readonly apiUrl  = 'api/courses'; // Caminho relativo ao arquivo JSON
 
   constructor(private http: HttpClient) {}
 
   getCourses() {
-    return this.http.get<Course[]>(this.apiUrl);
+    return this.http.get<Course[]>(this.apiUrl)
+    .pipe(
+      first(),
+      delay(0),
+      tap(courses => console.log(courses))
+    );
   }
 }
